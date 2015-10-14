f3417e3d2092a3073ca77f3147cd3f86a3094793
Administrator <183340093@qq.com>
2015-10-14 23:13:56 +0800

    增加xUtils第三方框架

--- app/src/main/java/com/zhm/duxiangle/MainActivity.java
+++ app/src/main/java/com/zhm/duxiangle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zhm.duxiangle;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,13 +14,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
