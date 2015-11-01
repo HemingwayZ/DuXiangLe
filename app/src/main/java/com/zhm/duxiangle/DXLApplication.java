6af8945945b010a0da0f28458e18e382bc6800d1
Administrator <183340093@qq.com>
2015-11-01 16:16:09 +0800

    初步加入了融云即时聊天的依赖包

--- app/src/main/java/com/zhm/duxiangle/DXLApplication.java
+++ app/src/main/java/com/zhm/duxiangle/DXLApplication.java
@@ -4,13 +4,20 @@ import android.app.Application;
 
 import com.baidu.mapapi.SDKInitializer;
 
+import io.rong.imkit.RongIM;
+
 public class DXLApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
-        SDKInitializer.initialize(this);
+        SDKInitializer.initialize(getApplicationContext());
+
+        /**
+         * 初始化融云
+         */
+        RongIM.init(this);
     }
 
 }
